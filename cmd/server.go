c5705c22e927c3ace314e359c48f66d68b12f005
HiulnaraPyvovar <lindworm1@gmail.com>
2025-07-01 09:20:24 +0000

    Implement Deployment Controller with basic reconciliation logic and tests

--- cmd/server.go
+++ cmd/server.go
@@ -6,13 +6,16 @@ import (
 	"os"
 
 	"github.com/google/uuid"
+	"github.com/hiulnarapyvovar/k8s-controller-tutorial/pkg/informer"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/valyala/fasthttp"
-	"github.com/hiulnarapyvovar/k8s-controller-tutorial/pkg/informer"
+	"github.com/yourusername/k8s-controller-tutorial/pkg/ctrl"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	ctrlruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 var serverPort int
@@ -32,6 +35,24 @@ var serverCmd = &cobra.Command{
 		}
 		ctx := context.Background()
 		go informer.StartDeploymentInformer(ctx, clientset)
+
+		// Start controller-runtime manager and controller
+		mgr, err := ctrlruntime.NewManager(ctrlruntime.GetConfigOrDie(), manager.Options{})
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to create controller-runtime manager")
+			os.Exit(1)
+		}
+		if err := ctrl.AddDeploymentController(mgr); err != nil {
+			log.Error().Err(err).Msg("Failed to add deployment controller")
+			os.Exit(1)
+		}
+		go func() {
+			log.Info().Msg("Starting controller-runtime manager...")
+			if err := mgr.Start(cmd.Context()); err != nil {
+				log.Error().Err(err).Msg("Manager exited with error")
+				os.Exit(1)
+			}
+		}()
 
 		handler := func(ctx *fasthttp.RequestCtx) {
 			requestID := uuid.New().String()
